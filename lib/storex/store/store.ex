0dda1fd9ce59aa6383f60bebaae519ff0948fc7d
Brett Sanders <brettrsanders@gmail.com>
2018-02-17 14:22:48 -0500

    make Book data dynamic

--- lib/storex/store/store.ex
+++ lib/storex/store/store.ex
@@ -1,14 +1,12 @@
 defmodule Storex.Store do
+  alias Storex.Repo
+  alias Storex.Store.Book
+
   def list_books() do
-  [
-      %{id: "1", title: "My first book", price: "15.9", description: "The first story", image_url: "/images/books/1.png"},
-      %{id: "2", title: "My second book", price: "25.9", description: "The second story", image_url: "/images/books/2.png" },
-      %{id: "3", title: "My third book", price: "35.9", description: "The third story", image_url: "/images/books/3.png"},
-      %{id: "4", title: "My fourth book", price: "45.9", description: "The fourth story", image_url: "/images/books/4.png"}
-    ]
+    Repo.all(Book)
   end
 
   def get_book(id) do
-    Enum.find(list_books(), fn(book) -> book.id == id end)
+    Repo.get(Book, id)
   end
 end
