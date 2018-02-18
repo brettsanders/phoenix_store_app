5b454978e6b5e33310a436e0b195e9aea96fab6d
Brett Sanders <brettrsanders@gmail.com>
2018-02-18 00:02:32 -0500

    add custom validation for max price

--- lib/storex/store/book.ex
+++ lib/storex/store/book.ex
@@ -17,5 +17,17 @@ defmodule Storex.Store.Book do
     book
     |> cast(attrs, [:title, :description, :price, :image_url])
     |> validate_required([:title, :description, :price, :image_url])
+    |> validate_max_price()
+  end
+
+  def validate_max_price(changeset) do
+    price = get_change(changeset, :price)
+
+    case Decimal.cmp(price, Decimal.new(99.99)) do
+      :gt ->
+        add_error(changeset, :price, "Price is not valid")
+       _ ->
+        changeset
+    end
   end
 end
