8b31b05e46c77e02b4733ac264f48a2d9784ad0b
Brett Sanders <brettrsanders@gmail.com>
2018-02-18 23:47:25 -0500

    Sales.create_cart/0

--- lib/storex/sales/sales.ex
+++ lib/storex/sales/sales.ex
@@ -2,4 +2,10 @@ defmodule Storex.Sales do
   import Ecto.Query, warn: false
   alias Storex.Repo
   alias Storex.Sales.{Cart, LineItem}
+
+  def create_cart(attrs \\ %{}) do
+    %Cart{}
+    |> Cart.changeset(attrs)
+    |> Repo.insert()
+  end
 end
--- test/storex/sales/sales_test.exs
+++ test/storex/sales/sales_test.exs
@@ -1,4 +1,12 @@
 defmodule Storex.SalesTest do
   use Storex.DataCase
   alias Storex.{Sales, Store}
+
+  describe "carts" do
+    alias Storex.Sales.Cart
+
+    test "create_cart/0 creates a cart" do
+      assert {:ok, %{}} = Sales.create_cart()
+    end
+  end
 end
