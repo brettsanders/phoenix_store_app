80d9d7802ae74f3f30ba661a869d1178548c32b6
Brett Sanders <brettrsanders@gmail.com>
2018-02-19 17:04:26 -0500

    add Sales.add_book_to_cart/2

--- test/storex/sales/sales_test.exs
+++ test/storex/sales/sales_test.exs
@@ -13,5 +13,24 @@ defmodule Storex.SalesTest do
       {:ok, cart} = Sales.create_cart()
       assert Sales.get_cart!(cart.id) == cart
     end
+
+    test "add_book_to_cart/2 creates or increments a line_item" do
+      {:ok, book} = Store.create_book(%{
+        title: "Title",
+        description: "Description",
+        image_url: "product.jpg",
+        price: "29.90"
+      })
+
+      {:ok, cart} = Sales.create_cart()
+      {:ok, line_item1} = Sales.add_book_to_cart(book, cart)
+      {:ok, line_item2} = Sales.add_book_to_cart(book, cart)
+
+      assert line_item1.quantity == 1
+      assert line_item1.book_id == book.id
+      assert line_item1.cart_id == cart.id
+      assert line_item1.id == line_item2.id
+      assert line_item2.quantity == 2
+    end
   end
 end
