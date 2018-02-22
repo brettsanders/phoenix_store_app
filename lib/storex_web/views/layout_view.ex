e1d649db644c90d81bb289040632ccde459cb315
Brett Sanders <brettrsanders@gmail.com>
2018-02-21 22:57:29 -0500

    format_price for dollars

--- lib/storex_web/views/layout_view.ex
+++ lib/storex_web/views/layout_view.ex
@@ -1,6 +1,7 @@
 defmodule StorexWeb.LayoutView do
   use StorexWeb, :view
   alias Storex.Sales
+  import StorexWeb.Helpers.PriceFormatter
 
   def items_count(conn) do
     StorexWeb.Plugs.ItemsCount.get(conn)
