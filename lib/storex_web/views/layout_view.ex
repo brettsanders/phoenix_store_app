0acf7f72501b179276d65b59bf7641011cee412f
Brett Sanders <brettrsanders@gmail.com>
2018-02-21 21:39:57 -0500

    add Plug for Total Price

--- lib/storex_web/views/layout_view.ex
+++ lib/storex_web/views/layout_view.ex
@@ -1,7 +1,12 @@
 defmodule StorexWeb.LayoutView do
   use StorexWeb, :view
+  alias Storex.Sales
 
   def items_count(conn) do
     StorexWeb.Plugs.ItemsCount.get(conn)
+  end
+
+  def items_total_price(conn) do
+    StorexWeb.Plugs.ItemsTotalPrice.get(conn)
   end
 end
